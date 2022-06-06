8b2a7004e310122c94ea1b0ba1ec013eaa825222
yesim <sityesim@gmail.com>
2022-06-06 21:43:14 +0300

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkItems=links.map((x) => (
+    <a key={x} href={"#" + x}>
+      {x}
+    </a>
+  ));
+  return <nav>{linkItems}</nav>;
 }
+
+ 
+  
+
 
 export default NavBar;
